849c24fc2b03bf648c6e0460885d0659aa953bc8
Quaomiuzzaman Kabbya <kabbya44@gmail.com>
2024-01-26 01:22:29 +0600

    print list

--- app.py
+++ app.py
@@ -9,8 +9,14 @@ class LinkedList:
         self.head = new_node
         self.tail = new_node
         self.length = 1
+    
+    def print_list(self):
+        temp = self.head 
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
 
 
 listed_1 = LinkedList(2)
 
-print(listed_1.head.value)
+listed_1.print_list()
