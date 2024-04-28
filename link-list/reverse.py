b4543809142adb6a6ae34b9907325d5d59ca4e70
Quaomiuzzaman Kabbya <kabbya44@gmail.com>
2024-04-28 19:52:33 +0600

    list-list-update

---
+++ link-list/reverse.py
@@ -0,0 +1,151 @@
+class Node:
+    def __init__(self,value) :
+        self.value = value 
+        self.next = None 
+
+class LinkList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node 
+        self.tail = new_node
+        self.length = 1 
+    
+    def append(self,value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+        self.length += 1
+
+    def print(self):
+        temp = self.head 
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next 
+
+    def pop(self):
+        if self.length == 0:
+            return  None
+        
+        pre = self.head 
+        temp = self.head 
+
+        while(temp.next):
+            pre = temp 
+            temp = temp.next 
+            
+        self.tail = pre 
+        self.tail.next = None 
+
+        self.length -= 1
+
+        if self.length == 0:
+            self.head = None 
+            self.tail = None 
+
+    def prepend(self,value):
+        new_node = Node(value)
+        if(self.length == 0):
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head 
+            self.head = new_node
+        
+        self.length += 1
+
+        return True
+    
+    def pop_first(self):
+        if self.length == 0:
+            return None 
+        
+        temp = self.head 
+        self.head = self.head.next 
+        temp.next = None 
+        self.length -= 1 
+
+        if self.length == 0:
+            self.tail = None 
+
+    def get(self,index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head 
+        for _ in range(index):
+            temp = temp.next 
+        return temp.value
+    
+    def set_value(self,index,value):
+        if index < 0:
+            return False 
+        
+        if index == 0:
+            self.head.value = value 
+        
+        temp = self.head 
+        current_index = 0; 
+
+        while temp.next:
+            if(current_index == index):
+                temp.value = value 
+                return True 
+            
+            temp = temp.next 
+            current_index += 1 
+        
+        return False 
+    
+    def remove(self,index):
+        if index < 0 or index >= self.length:
+            return None
+        
+        if index == 0:
+            return self.pop_first()
+        
+        if index == self.length - 1:
+            return self.pop()
+        
+        curr = self.head 
+        pre = None
+        count = 0
+
+        while curr and count != index:
+            pre = curr 
+            curr = curr.next 
+            count += 1 
+        
+        pre.next = curr.next 
+
+        return True
+    
+    def reverse(self):
+        temp = self.head 
+        self.head = self.tail 
+        self.tail = temp 
+        after = temp.next 
+        before = None 
+
+        for i in range(self.length):
+            after = temp.next 
+            temp.next = before 
+            before = temp 
+            temp = after
+
+        return True 
+    
+list = LinkList(4)
+list.append(3)
+list.append(7)
+list.prepend(1)
+list.prepend(2)
+list.prepend(7)
+list.print()
+print('reverse')
+list.reverse()
+list.print()
+
